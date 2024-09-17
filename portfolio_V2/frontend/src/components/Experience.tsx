74110a6946c0f57cd269c322519da51d7fd5e9e3
Riddy366 <ridwanabukar851@gmail.com>
2024-09-17 12:07:12 +0200

    Fjernet createdAt på experience(s) for det trenges ikke

--- portfolio_V2/frontend/src/components/Experience.tsx
+++ portfolio_V2/frontend/src/components/Experience.tsx
@@ -1,19 +1,17 @@
 type ExperienceProps = {
   title: string;
   description: string;
-  createdAt: string;
+
 };
 
 export default function Experience({
   title,
   description,
-  createdAt,
 }: ExperienceProps) {
   return (
     <div>
       <h3>{title}</h3>
       <p>{description}</p>
-      <small>{createdAt}</small>
     </div>
   );
 }
--- portfolio_V2/frontend/src/components/Experiences.tsx
+++ portfolio_V2/frontend/src/components/Experiences.tsx
@@ -4,9 +4,9 @@ export default function Experiences() {
   return (
     <>
       <h2>Experiences</h2>
-      <Experience title="Title1" description="desc1" createdAt={"Dato1"} />
-      <Experience title="Title2" description="desc2" createdAt={"Dato2"} />
-      <Experience title="Title2" description="desc2" createdAt={"Dato2"} />
+      <Experience title="Title1" description="desc1"/>
+      <Experience title="Title2" description="desc2"/>
+      <Experience title="Title2" description="desc2" />
     </>
   );
 }

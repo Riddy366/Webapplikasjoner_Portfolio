9f8932aa6fa1fad1b7b0fdde0a2fb9529abf29fc
Riddy366 <ridwanabukar851@gmail.com>
2024-09-17 14:12:42 +0200

    Mapping av experiences og projects
    Mapper projects og experiences fra listen som gjør det mer fleksibelt å hente dataene og legges i grensesnittet

--- portfolio_V2/frontend/src/components/Experiences.tsx
+++ portfolio_V2/frontend/src/components/Experiences.tsx
@@ -1,12 +1,27 @@
 import Experience from "./Experience";
 
 export default function Experiences() {
+
+  const experiences = [
+    {
+      title: "Title1",
+      description: "Desc1"
+    },
+    {
+      title: "Title2",
+      description: "Desc2"
+    },
+    {
+      title: "Title3",
+      description: "Desc3"
+    },
+  ]
   return (
     <>
       <h2>Experiences</h2>
-      <Experience title="Title1" description="desc1"/>
-      <Experience title="Title2" description="desc2"/>
-      <Experience title="Title2" description="desc2" />
+      {experiences.map((experiences, index) => (
+        <Experience key={index} title={experiences.title} description={experiences.description} />    
+      ))}
     </>
   );
 }
--- portfolio_V2/frontend/src/components/Projects.tsx
+++ portfolio_V2/frontend/src/components/Projects.tsx
@@ -1,24 +1,29 @@
 import Project from "./Project";
 
 export default function Projects() {
+    const projects = [
+        {
+            title: "Prosjekt2",
+            description: "Desc2",
+            createdAt: "dato idag"
+        },
+        {
+            title: "Prosjekt2",
+            description: "Desc2",
+            createdAt: "dato idag"
+        },
+        {
+            title: "Prosjekt3",
+            description: "Desc3",
+            createdAt: "dato idag"
+        }
+    ]
   return (
     <>
       <h2>Prosject</h2>
-      <Project
-        title={"Prosjekt1"}
-        description={"Desc1"}
-        createdAt={"dato idag"}
-      />
-      <Project
-        title={"Prosjekt2"}
-        description={"Desc2"}
-        createdAt={"dato idag"}
-      />
-      <Project
-        title={"Prosjekt3"}
-        description={"Desc3"}
-        createdAt={"dato idag"}
-      />
+      {projects.map((projects, index) => (
+      <Project key={index} title={projects.title} description={projects.description} createdAt={projects.createdAt}/>
+    ))}
     </>
   );
 }

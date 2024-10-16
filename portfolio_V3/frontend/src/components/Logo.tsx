aba5caa33ae0c92ba569c3812f9fa9c4411290b5
Riddy366 <ridwanabukar851@gmail.com>
2024-10-16 13:26:33 +0200

    Update Logo.tsx

--- portfolio_V3/frontend/src/components/Logo.tsx
+++ portfolio_V3/frontend/src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Logo() {
   return (
     <Link to="/">
-      <img src="public/bilder/logo.png" className="logo" alt="logo" />
+      <img src="/bilder/logo.png" className="logo" alt="logo" />
     </Link>
   );
 }
